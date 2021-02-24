02178d899a029c8f9352a21d5bb36bc3190f3172
sealfury <willspecker@gmail.com>
2021-02-24 11:16:56 +0100

    defines form submission routes

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/success', methods=['POST'])
+def success():
+    if request.method == 'POST':
+        email = request.form['email_name']
+        height = request.form['height_name']
+        return render_template("success.html")
+
 if __name__ == '__main__':
     app.debug = True
     app.run(port=3001)
